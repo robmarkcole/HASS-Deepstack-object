311748df9bd93b2b0881ce58c7aaf486f7917b53
robmarkcole <robmarkcole@gmail.com>
2019-01-15 06:46:37 +0000

    Working v0.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # HASS-Deepstack
-[Home Assistant](https://www.home-assistant.io/) custom component for using Deepstack face recognition &amp; object detection.
+[Home Assistant](https://www.home-assistant.io/) custom component for using Deepstack face recognition &amp; object detection. The component adds an `image_processing` entity where the state of the entity is the total number of faces that are found in the camera image.
 
 [Deepstack](https://www.deepquestai.com/insider/) is a service which runs in a docker container and exposes deep-learning models for performing facial recognition and object detection. There is no cost for using Deepstack, although you will need a machine with 8 GB RAM.
 
@@ -9,7 +9,7 @@ On your machine with docker, pull the latest image (approx. 2GB):
 sudo docker pull deepquestai/deepstack
 ```
 
-Run Deepstack with the face recognition service active run:
+Run Deepstack with the face recognition service active:
 ```
 sudo docker run -e VISION-FACE=True -v localstorage:/datastore -p 5000:5000 deepquestai/deepstack
 ```
@@ -23,3 +23,24 @@ This will expose the service on port `5000`.
 Place the `custom_components` folder in your configuration directory (or add its contents to an existing `custom_components` folder).
 
 Add to your Home-Assistant config:
+```yaml
+image_processing:
+  - platform: deepstack_face
+    ip_address: localhost
+    port: 5000
+    source:
+      - entity_id: camera.local_file
+        name: my_deepstack_name
+```
+Configuration variables:
+- **ip_address**: the ip address of your deepstack instance.
+- **port**: the port of your deepstack instance.
+- **source**: Must be a camera.
+- **name**: (Optional) A custom name for the the entity.
+
+### FAQ
+Q1: I get the following warning, is this normal?
+```
+2019-01-15 06:37:52 WARNING (MainThread) [homeassistant.loader] You are using a custom component for image_processing.deepstack_face which has not been tested by Home Assistant. This component might cause stability problems, be sure to disable it if you do experience issues with Home Assistant.
+```
+A1: Yes this is normal
---
+++ custom_components/image_processing/deepstack_face.py
@@ -0,0 +1,95 @@
+"""
+Component that will perform facial detection and identification via deepstack.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/image_processing.deepstack_face
+"""
+import base64
+import logging
+
+import requests
+import voluptuous as vol
+
+from homeassistant.const import (
+    ATTR_ENTITY_ID, ATTR_NAME)
+from homeassistant.core import split_entity_id
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.image_processing import (
+    PLATFORM_SCHEMA, ImageProcessingFaceEntity, ATTR_CONFIDENCE, CONF_SOURCE,
+    CONF_ENTITY_ID, CONF_NAME, DOMAIN)
+from homeassistant.const import (
+    CONF_IP_ADDRESS, CONF_PORT,
+    HTTP_BAD_REQUEST, HTTP_OK, HTTP_UNAUTHORIZED)
+
+_LOGGER = logging.getLogger(__name__)
+
+CLASSIFIER = 'deepstack'
+
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_IP_ADDRESS): cv.string,
+    vol.Required(CONF_PORT): cv.port,
+})
+
+
+def post_image(url, image):
+    """Post an image to the classifier."""
+    try:
+        response = requests.post(
+            url,
+            files={"image": image},
+            )
+        return response
+    except requests.exceptions.ConnectionError:
+        _LOGGER.error("ConnectionError: Is %s running?", CLASSIFIER)
+        return None
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the classifier."""
+    ip_address = config[CONF_IP_ADDRESS]
+    port = config[CONF_PORT]
+    entities = []
+    for camera in config[CONF_SOURCE]:
+        face_entity = FaceClassifyEntity(
+            ip_address, port, camera[CONF_ENTITY_ID], camera.get(CONF_NAME))
+        entities.append(face_entity)
+
+    add_devices(entities)
+
+
+class FaceClassifyEntity(ImageProcessingFaceEntity):
+    """Perform a face classification."""
+
+    def __init__(self, ip_address, port, camera_entity, name=None):
+        """Init with the API key and model id."""
+        super().__init__()
+        self._url_check = "http://{}:{}/v1/vision/face".format(
+            ip_address, port)
+        self._camera = camera_entity
+        if name:
+            self._name = name
+        else:
+            camera_name = split_entity_id(camera_entity)[1]
+            self._name = "{} {}".format(CLASSIFIER, camera_name)
+        self._matched = {}
+
+    def process_image(self, image):
+        """Process an image."""
+        response = post_image(
+            self._url_check, image)
+        if response:
+            if response.status_code == HTTP_OK:
+                self.total_faces = len(response.json()["predictions"])
+        else:
+            self.total_faces = None
+
+    @property
+    def camera_entity(self):
+        """Return camera entity id from process pictures."""
+        return self._camera
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
