77c1c492467be8489fe6ffe566c00b6e291d06cc
Robin Cole <robmarkcole@gmail.com>
2019-09-12 06:26:59 +0100

    Usee deepstack.core

--- custom_components/deepstack_object/image_processing.py
+++ custom_components/deepstack_object/image_processing.py
@@ -12,6 +12,8 @@ import os
 import requests
 import voluptuous as vol
 
+import deepstack.core as ds
+
 from homeassistant.const import ATTR_ENTITY_ID, ATTR_NAME
 from homeassistant.core import split_entity_id
 import homeassistant.helpers.config_validation as cv
@@ -37,7 +39,9 @@ _LOGGER = logging.getLogger(__name__)
 CLASSIFIER = "deepstack_object"
 CONF_TARGET = "target"
 CONF_SAVE_FILE_FOLDER = "save_file_folder"
+DEFAULT_API_KEY = ""
 DEFAULT_TARGET = "person"
+DEFAULT_TIMEOUT = 10
 EVENT_OBJECT_DETECTED = "image_processing.object_detected"
 EVENT_FILE_SAVED = "image_processing.file_saved"
 FILE = "file"
@@ -71,63 +75,12 @@ def draw_box(draw, prediction, text="", color=(255, 0, 0)):
         draw.text((left, abs(top - 15)), text, fill=color)
 
 
-def format_confidence(confidence):
-    """Takes a confidence from the API like 
-       0.55623 and returne 55.6 (%).
-    """
-    return round(float(confidence) * 100, 1)
-
-
-def get_confidences_above_threshold(confidences, threshold):
-    """Takes a list of confidences and returns those above a threshold."""
-    return [val for val in confidences if val >= threshold]
-
-
-def get_object_classes(predictions):
-    """
-    Get a list of the unique object classes predicted.
-    """
-    classes = [pred["label"] for pred in predictions]
-    return set(classes)
-
-
-def get_object_instances(predictions, target):
-    """
-    Return the number of instances of a target class.
-    """
-    targets_identified = [
-        format_confidence(pred["confidence"])
-        for pred in predictions
-        if pred["label"] == target
-    ]
-    return targets_identified
-
-
-def get_objects_summary(predictions):
-    """
-    Get a summary of the objects detected.
-    """
-    classes = get_object_classes(predictions)
-    return {
-        class_cat: len(get_object_instances(predictions, target=class_cat))
-        for class_cat in classes
-    }
-
-
-def post_image(url, image):
-    """Post an image to the classifier."""
-    try:
-        response = requests.post(url, files={"image": image})
-        return response
-    except requests.exceptions.ConnectionError:
-        _LOGGER.error("ConnectionError: Is %s running?", CLASSIFIER)
-        return None
-
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the classifier."""
     ip_address = config.get(CONF_IP_ADDRESS)
     port = config.get(CONF_PORT)
+    api_key = DEFAULT_API_KEY
+    timeout = DEFAULT_TIMEOUT
     target = config.get(CONF_TARGET)
     save_file_folder = config.get(CONF_SAVE_FILE_FOLDER)
     confidence = config.get(ATTR_CONFIDENCE)
@@ -140,6 +93,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         object_entity = ObjectClassifyEntity(
             ip_address,
             port,
+            api_key,
+            timeout,
             target,
             confidence,
             save_file_folder,
@@ -157,6 +112,8 @@ class ObjectClassifyEntity(ImageProcessingEntity):
         self,
         ip_address,
         port,
+        api_key,
+        timeout,
         target,
         confidence,
         save_file_folder,
@@ -165,7 +122,7 @@ class ObjectClassifyEntity(ImageProcessingEntity):
     ):
         """Init with the API key and model id."""
         super().__init__()
-        self._url_check = "http://{}:{}/v1/vision/detection".format(ip_address, port)
+        self._dsobject = ds.DeepstackObject(ip_address, port, api_key, timeout)
         self._target = target
         self._confidence = confidence
         self._camera = camera_entity
@@ -182,38 +139,33 @@ class ObjectClassifyEntity(ImageProcessingEntity):
 
     def process_image(self, image):
         """Process an image."""
-        response = post_image(self._url_check, image)
+        self._state = None
+        self._targets_confidences = []
+        self._predictions = {}
+        try:
+            self._dsobject.process_image_bytes(image)
+        except ds.DeepstackException as exc:
+            _LOGGER.error("Depstack error : %s", exc)
+            return
 
-        if response:
-            if response.status_code == HTTP_OK:
-                if not response.json()["success"]:
-                    _LOGGER.error("Error from Deepstack: %s", response.json()["error"])
-                elif response.json()[
-                    "success"
-                ]:  # Get 200 code even if incorrect API key
-                    predictions_json = response.json()["predictions"]
-                    self._targets_confidences = get_object_instances(
-                        predictions_json, self._target
-                    )
-                    self._state = len(
-                        get_confidences_above_threshold(
-                            self._targets_confidences, self._confidence
-                        )
-                    )
-                    self._predictions = get_objects_summary(predictions_json)
-                    self.fire_prediction_events(predictions_json, self._confidence)
-                    if hasattr(self, "_save_file_folder") and self._state > 0:
-                        self.save_image(
-                            image,
-                            predictions_json,
-                            self._target,
-                            self._save_file_folder,
-                        )
+        predictions = self._dsobject.predictions.copy()
 
-        else:
-            self._state = None
-            self._targets_confidences = []
-            self._predictions = {}
+        if len(predictions) > 0:
+            raw_confidences = ds.get_label_confidences(predictions, self._target)
+            self._targets_confidences = [
+                ds.format_confidence(confidence) for confidence in raw_confidences
+            ]
+            self._state = len(
+                ds.get_confidences_above_threshold(
+                    self._targets_confidences, self._confidence
+                )
+            )
+            self._predictions = ds.get_objects_summary(predictions)
+            self.fire_prediction_events(predictions, self._confidence)
+            if hasattr(self, "_save_file_folder") and self._state > 0:
+                self.save_image(
+                    image, predictions, self._target, self._save_file_folder
+                )
 
     def save_image(self, image, predictions_json, target, directory):
         """Save a timestamped image with bounding boxes around targets."""
@@ -224,7 +176,7 @@ class ObjectClassifyEntity(ImageProcessingEntity):
         draw = ImageDraw.Draw(img)
 
         for prediction in predictions_json:
-            prediction_confidence = format_confidence(prediction["confidence"])
+            prediction_confidence = ds.format_confidence(prediction["confidence"])
             if (
                 prediction["label"] == target
                 and prediction_confidence >= self._confidence
@@ -246,14 +198,14 @@ class ObjectClassifyEntity(ImageProcessingEntity):
         """Fire events based on predictions if above confidence threshold."""
 
         for prediction in predictions_json:
-            if format_confidence(prediction["confidence"]) > confidence:
+            if ds.format_confidence(prediction["confidence"]) > confidence:
                 self.hass.bus.fire(
                     EVENT_OBJECT_DETECTED,
                     {
                         "classifier": CLASSIFIER,
                         ATTR_ENTITY_ID: self.entity_id,
                         OBJECT: prediction["label"],
-                        ATTR_CONFIDENCE: format_confidence(prediction["confidence"]),
+                        ATTR_CONFIDENCE: ds.format_confidence(prediction["confidence"]),
                     },
                 )
 
